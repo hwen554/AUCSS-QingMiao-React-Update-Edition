0eda9f6e555f930355bc2bceac95a48237b39c25
hwen554 <78581470+hwen554@users.noreply.github.com>
2022-02-13 23:06:28 +1300

    update

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../../App.css';
-
+import '../SignUp.css';
 import {FaUser} from 'react-icons/fa';
 import {FaLock} from 'react-icons/fa';
 import { Button } from '@douyinfe/semi-ui';
@@ -9,7 +9,8 @@ export default function SignUp() {
   return (
     <>
     <video src='/videos/video-1.mp4' autoPlay loop unmuted />
-    <div className='container'>
+    <div className='body'>
+    <div className='main_div'>
       <div className='title'>Member Login</div>
       <form action='#'>
         <div className='input_box'>
@@ -20,9 +21,24 @@ export default function SignUp() {
           <input type='text' placeholder='Enter your password' required/>
           <div className='icon'><FaLock/></div>
         </div>
+        <div className='option_div'>
+          <div className='check_box'>
+            <input type='checkbox'/>
+            <span>Remember me</span>
+          </div>
+          <div className='forgot_div'>
+            <a href='#'>Forgot Password</a>
+          </div>
+        </div>
+        <div className='input_box button'>
+          <input type='submit' value='Login'/>
+        </div>
+        <div className='sign_up'>
+          Not a member?<a href='#'>Sign Up Now</a>
+        </div>
       </form>
     </div>
-    
+    </div>
     </>
     
     
