2986c6d10330e7092ae7ec76f44d70aab1b387ce
Gavin Weng <78581470+hwen554@users.noreply.github.com>
2023-01-03 18:36:18 +1300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,13 +14,13 @@ function App() {
     <>
       <Router>
         <Navbar>
-        <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/services' component={Services} />
-          <Route path='/products' component={Products} />
-          <Route path='/sign-up' component={SignUp} />
-          <Route path='/qingmiao' component={Qingmiao}/>
-        </Switch>
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/services' component={Services} />
+            <Route path='/products' component={Products} />
+            <Route path='/sign-up' component={SignUp} />
+            <Route path='/qingmiao' component={Qingmiao}/>
+          </Switch>
         
       </Router>
     </>
