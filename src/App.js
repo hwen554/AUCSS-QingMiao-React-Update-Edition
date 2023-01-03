03fdd0f68eba91a64caa0fb7518e3dbf2786dbd8
Gavin Weng <78581470+hwen554@users.noreply.github.com>
2023-01-03 18:35:58 +1300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <>
       <Router>
-        <Navbar >
+        <Navbar>
         <Switch>
           <Route path='/' exact component={Home} />
           <Route path='/services' component={Services} />
